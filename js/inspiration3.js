d753d2445d32f0af085920463e58887fd09ed48e
Cienna-Kim <kimg379@newschool.edu>
2021-04-10 16:03:25 +0900

    Create inspiration3.js

---
+++ js/inspiration3.js
@@ -0,0 +1,54 @@
+// runs once and setups the scene
+function setup(){
+	createCanvas (600, 600);
+}
+
+// runs forever
+function draw(){
+// cliff
+	var rectWidth = 50;
+	var rectHeight = 50;
+	background("#214371");
+
+// scent bubbles
+	noStroke();
+ 	fill("#000000");
+ 	ellipse(350,350,100,100);
+ 	noStroke();
+ 	fill("#003BA9");
+ 	ellipse(200,450,200,200);
+ 	noStroke();
+ 	fill("#90DFFF");
+ 	ellipse(500,150,80,160);
+ 	noStroke();
+ 	fill("#707BFF");
+ 	ellipse(400,80,200,300);
+ 	noStroke();
+ 	fill("#00A2FF");
+ 	ellipse(50,450,70,270);
+ 	noStroke();
+ 	fill("#7CD3E5");
+ 	ellipse(300,500,250,400);
+ 	noStroke();
+ 	fill("#4F9AFF");
+ 	ellipse(330,40,200,60);
+ 	noStroke();
+ 	fill("#7CD3E5");
+ 	ellipse(20,110,350,200);
+
+ 	noStroke();
+ 	fill("#000000");
+ 	ellipse(450,350,50,100);
+ 	noStroke();
+ 	fill("#0ABFC8");
+ 	ellipse(200,250,200,40);
+ 	noStroke();
+ 	fill("#0A19C8");
+ 	ellipse(520,250,40,30);
+ 	noStroke();
+ 	fill("#707BFF");
+ 	ellipse(40,80,20,60);
+ 	noStroke();
+ 	fill("#7B8CC9");
+ 	ellipse(550,550,200,160);
+}
