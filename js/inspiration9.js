47be155ab69853ca0575459aa3378f5ca6480fe8
Cienna-Kim <kimg379@newschool.edu>
2021-04-25 19:52:15 +0900

    Create inspiration9.js

---
+++ js/inspiration9.js
@@ -0,0 +1,196 @@
+const ln = 100;
+const xPos = 100;
+const yPos = 100;
+let g = 100;
+function setup() {
+  createCanvas(700, 700);
+  noLoop();
+  noStroke();
+}
+
+function draw() {
+  background("#00bfff");
+  
+// line 1
+  fill (0, 0, 0)
+  ellipse(xPos *0.5, yPos *0, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *0.5, yPos *0.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *0.5, yPos *1, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *0.5, yPos *3, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *0.5, yPos *5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *0.5, yPos *7, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *0.5, yPos *1.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *0.5, yPos *3.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *0.5, yPos *5.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *0.5, yPos *2, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *0.5, yPos *4, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *0.5, yPos *6, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *0.5, yPos *2.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *0.5, yPos *4.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *0.5, yPos *6.5, ln, ln);
+  
+// line 2
+  fill (0, 0, 0)
+  ellipse(xPos *2.5, yPos *0, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *2.5, yPos *0.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *2.5, yPos *1, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *2.5, yPos *3, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *2.5, yPos *5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *2.5, yPos *7, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *2.5, yPos *1.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *2.5, yPos *3.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *2.5, yPos *5.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *2.5, yPos *2, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *2.5, yPos *4, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *2.5, yPos *6, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *2.5, yPos *2.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *2.5, yPos *4.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *2.5, yPos *6.5, ln, ln);
+  
+// line 3
+  fill (0, 0, 0)
+  ellipse(xPos *4.5, yPos *0, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *4.5, yPos *0.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *4.5, yPos *1, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *4.5, yPos *3, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *4.5, yPos *5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *4.5, yPos *7, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *4.5, yPos *1.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *4.5, yPos *3.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *4.5, yPos *5.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *4.5, yPos *2, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *4.5, yPos *4, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *4.5, yPos *6, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *4.5, yPos *2.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *4.5, yPos *4.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *4.5, yPos *6.5, ln, ln);
+  
+// line 4
+  fill (0, 0, 0)
+  ellipse(xPos *6.5, yPos *0, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *6.5, yPos *0.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *6.5, yPos *1, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *6.5, yPos *3, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *6.5, yPos *5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *6.5, yPos *7, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *6.5, yPos *1.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *6.5, yPos *3.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *6.5, yPos *5.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *6.5, yPos *2, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *6.5, yPos *4, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *6.5, yPos *6, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *6.5, yPos *2.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *6.5, yPos *4.5, ln, ln);
+  fill (0, 0, 0)
+  ellipse(xPos *6.5, yPos *6.5, ln, ln);
+
+// bigger circles
+  
+  
+  fill (0, 0, 0, 50)
+  ellipse(xPos *1.5, yPos *0, 2*ln, 2*ln);
+  fill (0, 0, 0, 100)
+  ellipse(xPos *1.5, yPos *2, 2*ln, 2*ln);
+  fill (0, 0, 0, 50)
+  ellipse(xPos *1.5, yPos *4, 2*ln, 2*ln);
+  fill (0, 0, 0, 100)
+  ellipse(xPos *1.5, yPos *6, 2*ln, 2*ln);
+  fill (0, 0, 0, 50)
+  ellipse(xPos *1.5, yPos *1, 2*ln, 2*ln);
+  fill (0, 0, 0, 100)
+  ellipse(xPos *1.5, yPos *3, 2*ln, 2*ln);
+  fill (0, 0, 0, 50)
+  ellipse(xPos *1.5, yPos *5, 2*ln, 2*ln);
+  fill (0, 0, 0, 100)
+  ellipse(xPos *1.5, yPos *7, 2*ln, 2*ln);
+  
+  fill (0, 0, 0, 100)
+  ellipse(xPos *3.5, yPos *0, 2*ln, 2*ln);
+  fill (0, 0, 0, 50)
+  ellipse(xPos *3.5, yPos *2, 2*ln, 2*ln);
+  fill (0, 0, 0, 100)
+  ellipse(xPos *3.5, yPos *4, 2*ln, 2*ln);
+  fill (0, 0, 0, 50)
+  ellipse(xPos *3.5, yPos *6, 2*ln, 2*ln);
+  fill (0, 0, 0, 100)
+  ellipse(xPos *3.5, yPos *1, 2*ln, 2*ln);
+  fill (0, 0, 0, 50)
+  ellipse(xPos *3.5, yPos *3, 2*ln, 2*ln);
+  fill (0, 0, 0, 100)
+  ellipse(xPos *3.5, yPos *5, 2*ln, 2*ln);
+  fill (0, 0, 0, 50)
+  ellipse(xPos *3.5, yPos *7, 2*ln, 2*ln);
+
+  fill (0, 0, 0, 50)
+  ellipse(xPos *5.5, yPos *0, 2*ln, 2*ln);
+  fill (0, 0, 0, 100)
+  ellipse(xPos *5.5, yPos *2, 2*ln, 2*ln);
+  fill (0, 0, 0, 50)
+  ellipse(xPos *5.5, yPos *4, 2*ln, 2*ln);
+  fill (0, 0, 0, 100)
+  ellipse(xPos *5.5, yPos *6, 2*ln, 2*ln);
+  fill (0, 0, 0, 50)
+  ellipse(xPos *5.5, yPos *1, 2*ln, 2*ln);
+  fill (0, 0, 0, 100)
+  ellipse(xPos *5.5, yPos *3, 2*ln, 2*ln);
+  fill (0, 0, 0, 50)
+  ellipse(xPos *5.5, yPos *5, 2*ln, 2*ln);
+  fill (0, 0, 0, 100)
+  ellipse(xPos *5.5, yPos *7, 2*ln, 2*ln);
+
+}
