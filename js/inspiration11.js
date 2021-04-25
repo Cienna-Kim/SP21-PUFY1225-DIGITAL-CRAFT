898c16083a8d511858ebe2f93c6502614b1aeadf
Cienna-Kim <kimg379@newschool.edu>
2021-04-25 20:10:39 +0900

    Create inspiration11.js

---
+++ js/inspiration11.js
@@ -0,0 +1,105 @@
+const ln = 100;
+const xPos = 100;
+const yPos = 100;
+let g = 100;
+function setup() {
+  createCanvas(600, 600);
+  noLoop();
+  noStroke();
+}
+
+function draw() {
+  background("#bad3ff");
+
+//line 1
+  fill (g, 0, g)
+  rect (0, 0, 100, 100)
+  fill (g, 0, g)
+  rect (100, 0, 80, 100)
+  fill (g, 0, g)
+  rect (200, 0, 100, 100)
+  fill (g, 0, g)
+  rect (300, 0, 80, 100)
+  fill (g, 0, g)
+  rect (400, 0, 100, 100)
+  fill (g, 0, g)
+  rect (520, 0, 80, 100)
+  
+//line 2
+  fill (0, g*0.4, g)
+  rect (0, 100, 30, 100)
+  fill (0, g*0.4, g)
+  rect (50, 100, 80, 100)
+  fill (0, g*0.4, g)
+  rect (150, 100, 100, 100)
+  fill (0, g*0.4, g)
+  rect (250, 100, 80, 100)
+  fill (0, g*0.4, g)
+  rect (350, 100, 100, 100)
+  fill (0, g*0.4, g)
+  rect (450, 100, 80, 100)
+  fill (0, g*0.4, g)
+  rect (550, 100, 100, 100)
+  
+//line 3
+  fill (g, 0, g)
+  rect (0, 200, 100, 100)
+  fill (g, 0, g)
+  rect (100, 200, 80, 100)
+  fill (g, 0, g)
+  rect (200, 200, 100, 100)
+  fill (g, 0, g)
+  rect (300, 200, 80, 100)
+  fill (g, 0, g)
+  rect (400, 200, 100, 100)
+  fill (g, 0, g)
+  rect (520, 200, 80, 100)
+  
+//line 4
+  fill (0, g*0.4, g)
+  rect (0, 300, 30, 100)
+  fill (0, g*0.4, g)
+  rect (50, 300, 80, 100)
+  fill (0, g*0.4, g)
+  rect (150, 300, 100, 100)
+  fill (0, g*0.4, g)
+  rect (250, 300, 80, 100)
+  fill (0, g*0.4, g)
+  rect (350, 300, 100, 100)
+  fill (0, g*0.4, g)
+  rect (450, 300, 80, 100)
+  fill (0, g*0.4, g)
+  rect (550, 300, 100, 100)
+  
+//line 5
+  fill (g, 0, g)
+  rect (0, 400, 100, 100)
+  fill (g, 0, g)
+  rect (100, 400, 80, 100)
+  fill (g, 0, g)
+  rect (200, 400, 100, 100)
+  fill (g, 0, g)
+  rect (300, 400, 80, 100)
+  fill (g, 0, g)
+  rect (400, 400, 100, 100)
+  fill (g, 0, g)
+  rect (520, 400, 80, 100)
+  
+//line 6
+  fill (0, g*0.4, g)
+  rect (0, 500, 30, 100)
+  fill (0, g*0.4, g)
+  rect (50, 500, 80, 100)
+  fill (0, g*0.4, g)
+  rect (150, 500, 100, 100)
+  fill (0, g*0.4, g)
+  rect (250, 500, 80, 100)
+  fill (0, g*0.4, g)
+  rect (350, 500, 100, 100)
+  fill (0, g*0.4, g)
+  rect (450, 500, 80, 100)
+  fill (0, g*0.4, g)
+  rect (550, 500, 100, 100)
+
+  
+}
